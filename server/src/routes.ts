394b5125ef2e9758ffa247a941cf546886b60c36
Joao Marcelo Nunes Chaves <jmarcelonunes738@gmail.com>
2020-06-02 18:13:53 -0300

    Creates get, show and create methods for Points and get method for Items

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,16 @@
 import express from 'express';
+import PointsControllers from './controllers/PointsControllers';
+import ItemsControllers from './controllers/ItemsControllers';
+
 
 const routes = express.Router()
+const pointsControllers = new PointsControllers();
+const itemsControllers = new ItemsControllers();
 
-routes.get('/', (request, response) => {
-    return response.json({message: "ola"});
-});
+routes.get('/items', itemsControllers.index);
+
+routes.post('/points', pointsControllers.create);
+
+routes.get('/points/:id', pointsControllers.show)
 
 export default routes
